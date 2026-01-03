b304142893e380dcc9da5a448901fc2688e6c454
Ngoc-Nhan <nhanvtn7501@ut.edu.vn>
2026-01-03 13:37:36 +0700

    home

---
+++ lib/pages/plan.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Plan extends StatefulWidget {
+  const Plan({super.key});
+
+  @override
+  State<Plan> createState() => _PlanState();
+}
+
+class _PlanState extends State<Plan> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../models/mood_selector.dart';
+import 'package:typewritertext/typewritertext.dart';
+import '../../models/choice_plan.dart';
 
 String getGreeting() {
   final hour = DateTime.now().hour;
@@ -17,27 +19,27 @@ String getGreeting() {
   return '$greeting ! ';
 }
 
-Widget iconSection() {
-  return Container(
-    margin: const EdgeInsets.symmetric(horizontal: 20),
-    padding: const EdgeInsets.symmetric(vertical: 20),
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(24),
-      boxShadow: [
-        BoxShadow(color: Colors.black12, blurRadius: 12, offset: Offset(0, 6)),
-      ],
-    ),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: const [
-        Icon(Icons.home, size: 30),
-        Icon(Icons.favorite, size: 30),
-        Icon(Icons.person, size: 30),
-      ],
-    ),
-  );
-}
+// Widget iconSection() {
+//   return Container(
+//     margin: const EdgeInsets.symmetric(horizontal: 20),
+//     padding: const EdgeInsets.symmetric(vertical: 20),
+//     decoration: BoxDecoration(
+//       color: Colors.white,
+//       borderRadius: BorderRadius.circular(24),
+//       boxShadow: [
+//         BoxShadow(color: Colors.black12, blurRadius: 12, offset: Offset(0, 6)),
+//       ],
+//     ),
+//     child: Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceAround,
+//       children: const [
+//         Icon(Icons.home, size: 30),
+//         Icon(Icons.favorite, size: 30),
+//         Icon(Icons.person, size: 30),
+//       ],
+//     ),
+//   );
+// }
 
 class HomeScreen extends StatelessWidget {
   final String name;
@@ -51,6 +53,8 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         children: [
           Stack(
+            // khong cat phan thua
+            clipBehavior: Clip.none,
             children: [
               SafeArea(
                 bottom: false,
@@ -143,19 +147,60 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               Positioned(
-                top: 180,
+                top: 160,
                 left: 0,
                 right: 0,
-                child: Image.asset(
-                  'assets/images/wave.png',
-                  fit: BoxFit.cover,
-                  height: 180,
+                bottom: 0,
+                child: IgnorePointer(
+                  child: Image.asset(
+                    'assets/images/wave.png',
+                    fit: BoxFit.cover,
+                    alignment: Alignment.topCenter,
+                    height: 90, // chỉ lấy phần trên
+                    width: double.infinity,
+                  ),
                 ),
               ),
-              Positioned(top: 220, left: 0, right: 0, child: iconSection()),
+              // Positioned(
+              //   top: 193,
+              //   left: 20,
+              //   right: 20,
+              //   child: const MoodSelector(),
+              // ),
             ],
           ),
-          const MoodSelector(),
+
+          // SizedBox(height: 100),
+          // const SizedBox(height: 100),
+          // const MoodSelector(),
+          Transform.translate(
+            offset: const Offset(0, -18), // 👈 số âm = đẩy lên
+            child: const MoodSelector(),
+          ),
+
+          Container(
+            padding: EdgeInsets.only(left: 60),
+            width: double.infinity,
+
+            child: TypeWriter.text(
+              'How are you felling today ?',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              duration: const Duration(milliseconds: 50),
+            ),
+          ),
+          // SizedBox(height: 20),
+
+          // SizedBox(height: 50),
+          // const SizedBox(height: 1),
+
+          // MoodSelector(),
+
+          // TypeWriter.text(
+          //   'How are you feeling today ?',
+          //   style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+          //   duration: const Duration(milliseconds: 50),
+          // ),
+          ChoiceOption(),
         ],
       ),
     );
